77d3c35f71743d52319e0b3396c95ea64eeaad56
Parker Siroishka <parkersir10@hotmail.com>
2021-01-24 18:36:21 -0700

    Customized Landing Page More

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,3 +4,7 @@ const URL = location.origin; // root domain
 $("#go-p0").on("click", e => {
     window.location.assign(`${URL}/pages/p0.html`);
 });
+
+$("#src-p0").on("click", e => {
+    window.location.assign(`https://drive.google.com/file/d/1H5PZpiWlJfv92e5IhVExGJdY1EqBo_t6/view?usp=sharing`);
+});
