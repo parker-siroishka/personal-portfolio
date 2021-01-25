8c4ccee5cd9adb3ff4f8525634f42e75e4d80b80
Parker Siroishka <parkersir10@hotmail.com>
2021-01-24 22:44:08 -0700

    Some minor styling tweaks on p0 and on portfolio. (Trying to make it look more like my own.)

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -8,3 +8,7 @@ $("#go-p0").on("click", e => {
 $("#src-p0").on("click", e => {
     window.location.assign(`https://drive.google.com/file/d/1H5PZpiWlJfv92e5IhVExGJdY1EqBo_t6/view?usp=sharing`);
 });
+
+$("#det-p0").on("click", e => {
+    window.location.assign(`${URL}/pages/p0Details.html`);
+});
