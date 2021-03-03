4f1839fe442f381f7878ae57ade5f77f87bfdd2e
Parker Siroishka <parkersir10@hotmail.com>
2021-03-02 18:31:11 -0700

    Testing Gain

--- public/scripts/p1.js
+++ public/scripts/p1.js
@@ -96,7 +96,6 @@ function init() {
     }
 
     var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    var gainNode = audioCtx.createGain();
     var source;
 
     var analyser = audioCtx.createAnalyser();
@@ -104,7 +103,7 @@ function init() {
     analyser.maxDecibels = -10;
     analyser.smoothingTimeConstant = 0.85;
 
-    
+    var voiceVolume = audioCtx.createGain();
 
     if (navigator.mediaDevices.getUserMedia) {
         console.log('getUserMedia supported.');
@@ -113,25 +112,26 @@ function init() {
            .then(
              function(stream) {
                 source = audioCtx.createMediaStreamSource(stream);
-                //source.connect(gainNode);
-                source.connect(analyser);
-                source.connect(audioCtx.destination);
+                
+                //voiceVolume.connect(analyser);
+                source.connect(voiceVolume).connect(analyser).connect(audioCtx.destination);;
+                beginRecording();
+   
+                
            })
            .catch( function(err) { console.log('The following gUM error occured: ' + err);})
      } else {
         console.log('getUserMedia not supported on your browser!');
      }
 
-    
-    
-    beginRecording();
-
      function beginRecording() {
         analyser.fftSize = 1024; // power of 2, between 32 and max unsigned integer
         var bufferLength = analyser.fftSize;
+
         var freqBinDataArray = new Uint8Array(bufferLength);
 
         var checkAudio = function() {
+            voiceVolume.gain.setValueAtTime(GAIN, audioCtx.currentTime);
             analyser.getByteFrequencyData(freqBinDataArray);
             VOLUME = (CAM_PRIMED) ? getRMS(freqBinDataArray) : 0;
             bar.animate(VOLUME/30, {
@@ -191,15 +191,11 @@ webcam.start()
 
 function snapPhoto() {
     let picture = webcam.snap();
-    gainNode.gain.setValueAtTime(1, audioCtx.currentTime);
     document.querySelector('#download-photo').href = picture;
     CANVAS_FILLED = true;
     $('html, body').animate({
         scrollTop: $("#resetCam").offset().top
     }, 1000);
-    setTimeout(function(){
-        gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
-    },1000);
 }
 
 function primeCamera() {
