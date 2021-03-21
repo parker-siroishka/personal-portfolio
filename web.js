5e679d3c7362fbfd226904c8281a18669064707f
Parker Siroishka <parkersir10@hotmail.com>
2021-03-21 13:00:59 -0600

    Testing RTC

--- web.js
+++ web.js
@@ -1,13 +1,43 @@
-
-
-var path = require('path'); 		// some convenient dir/path functions
-var express = require('express');	// use the express module
-var app = express();			// this is our express.js instance
+const path = require('path'); 		// some convenient dir/path functions
+const express = require('express');	// use the express module
+const ws = require("ws");
 const PORT = process.env.PORT || 5000 // Port should be 5000 by default
+const DOMAIN = "https://parker-siroishka-portfolio.herokuapp.com/";
 
-app.use(express.static(path.join(__dirname, 'public'))) // lets us serve static files from the "public" directory
-	.get('/', (req, res) => { // respond to HTTP GET request. '/' is the root endpoint.
-res.sendFile(path.join(__dirname, 'public/pages/index.html')) // serve the landing static page
+var app = express();			// this is our express.js instance
+
+const MessageType = {
+    SERVER_INFO: 0,
+    CLIENT1: 1,
+    CLIENT2: 2,
+    CALL_REQUEST: 3
+};
+
+var wsServer = new ws.Server({ noServer: true });
+
+app
+    .use(express.static(path.join(__dirname, "public")))
+    .use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", DOMAIN);
+        res.header(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        next();
     })
+    .get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "public/pages/index.html"));
+    })
+    .listen(PORT)
+    .on("upgrade", (request, socket, head) => {
+        wsServer.handleUpgrade(request, socket, head, (socket) => {
+            wsServer.emit("connection", socket, request);
+        });
+    });
 
-.listen(PORT); // keep the server listening on port 5000
+// app.use(express.static(path.join(__dirname, 'public'))) // lets us serve static files from the "public" directory
+// 	.get('/', (req, res) => { // respond to HTTP GET request. '/' is the root endpoint.
+// res.sendFile(path.join(__dirname, 'public/pages/index.html')) // serve the landing static page
+//     })
+
+// .listen(PORT); // keep the server listening on port 5000
