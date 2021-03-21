9eac939e63116f8a2159e5bbb292fbaf44d6bb2b
Parker Siroishka <parkersir10@hotmail.com>
2021-03-21 15:28:20 -0600

    Trying getUserMedia support

--- public/scripts/p2.js
+++ public/scripts/p2.js
@@ -53,24 +53,67 @@ callBtn.on("click", () => {
 });
 
 function getWebcam() {
-    if (navigator.getUserMedia) {
-        navigator.getUserMedia({
-                video: true,
-                audio: true,
-            },
-            (stream) => {
-                // success
+
+    // Older browsers might not implement mediaDevices at all, so we set an empty object first
+    if (navigator.mediaDevices === undefined) {
+        navigator.mediaDevices = {};
+      }
+  
+      // Some browsers partially implement mediaDevices. We can't just assign an object
+      // with getUserMedia as it would overwrite existing properties.
+      // Here, we will just add the getUserMedia property if it's missing.
+      if (navigator.mediaDevices.getUserMedia === undefined) {
+        navigator.mediaDevices.getUserMedia = function(constraints) {
+    
+          // First get ahold of the legacy getUserMedia, if present
+          var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    
+          // Some browsers just don't implement it - return a rejected promise with an error
+          // to keep a consistent interface
+          if (!getUserMedia) {
+            return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+          }
+
+
+    
+          // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+          return new Promise(function(resolve, reject) {
+            getUserMedia.call(navigator, constraints, resolve, reject);
+          });
+        }
+      }
+    
+      if (navigator.mediaDevices.getUserMedia) {
+        console.log('getUserMedia supported.');
+        var constraints = {video: true, audio: true}
+        navigator.mediaDevices.getUserMedia (constraints)
+           .then(
+             function(stream) {
                 localStream = stream;
                 localVid.prop("srcObject", stream);
-            },
-            (error) => {
-                // error
-                console.error(error);
-            }
-        );
-    } else {
-        alert("Your browser does not support getUserMedia API");
-    }
+           })
+           .catch( function(err) { console.log('The following gUM error occured: ' + err);})
+     } else {
+        console.log('getUserMedia not supported on your browser!');
+     }
+    // if (navigator.getUserMedia) {
+    //     navigator.getUserMedia({
+    //             video: true,
+    //             audio: true,
+    //         },
+    //         (stream) => {
+    //             // success
+    //             localStream = stream;
+    //             localVid.prop("srcObject", stream);
+    //         },
+    //         (error) => {
+    //             // error
+    //             console.error(error);
+    //         }
+    //     );
+    // } else {
+    //     alert("Your browser does not support getUserMedia API");
+    // }
 }
 
 function start(isCaller) {
