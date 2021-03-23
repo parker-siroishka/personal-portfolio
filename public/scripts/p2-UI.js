67435dbf8f00bef4960e1c41e0bea3023001efdc
Parker Siroishka <parkersir10@hotmail.com>
2021-03-22 19:37:57 -0600

    Began Client Specific UIs

--- public/scripts/p2-UI.js
+++ public/scripts/p2-UI.js
@@ -1,13 +1,25 @@
 
 var isRhys = document.getElementById('btn-client1');
 var isGparent = document.getElementById('btn-client2');
+var rhysView = document.getElementById('rhys-view');
+var gparentView = document.getElementById('gparent-view');
+var localVideo = document.getElementById('localVideo');
+var remoteVideo = document.getElementById('remoteVideo');
 
 function showRhysUI(){
-    console.log("I AM RHYS");
+    isRhys.innerText = "Connected as Rhys";
+    isGparent.style.display = "none";
+    rhysView.style.display = "block";
+    localVideo.style.display = "block";
+    remoteVideo.style.display = "block";
 }
 
 function showGparentUI(){
-    console.log("I AM GPARENT");
+    isGparent.innerText = "Connected as Oma & Opa";
+    isRhys.style.display = "none";
+    gparentView.style.display = "block";
+    localVideo.style.display = "block";
+    remoteVideo.style.display = "block";
 }
 
 isRhys.addEventListener('click', showRhysUI);
